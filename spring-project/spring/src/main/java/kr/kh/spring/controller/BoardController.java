b08a477f4a57e696cc7c24769f624563a59ebda9
Won0721 <133324320+Won0721@users.noreply.github.com>
2023-09-04 12:39:41 +0900

    게시글 조회

--- spring-project/spring/src/main/java/kr/kh/spring/controller/BoardController.java
+++ spring-project/spring/src/main/java/kr/kh/spring/controller/BoardController.java
@@ -1,5 +1,7 @@
 package kr.kh.spring.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import kr.kh.spring.pagination.Criteria;
+import kr.kh.spring.pagination.PageMaker;
 import kr.kh.spring.service.BoardService;
 import kr.kh.spring.vo.BoardVO;
 import kr.kh.spring.vo.MemberVO;
@@ -22,9 +26,18 @@ public class BoardController {
 		BoardService boardService;
 		
 		@GetMapping("/list")
-		public String list() {
+		public String list(Model model,Criteria cri) {
+			cri.setPerPageNum(2);
+			//현재 페이지에 맞는 게시글을 가져와야함
+			List<BoardVO> list = boardService.getBoardList(cri);
+			int totalCount = boardService.getTotalCount(cri);
+			PageMaker pm = new PageMaker(3, cri, totalCount);
+			
+			model.addAttribute("pm",pm);
+			model.addAttribute("list",list);
 			return "/board/list";
 		}
+		
 		
 		@GetMapping("/insert")
 		public String insert(HttpServletRequest request) {
